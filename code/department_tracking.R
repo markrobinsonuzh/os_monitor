ca4f9f6c65245c275252abcba1522ed2ddc7e2db
markrobinsonuzh <mark.robinson@imls.uzh.ch>
2020-06-20 07:14:54 +0200

    add files

--- code/department_tracking.R
+++ code/department_tracking.R
@@ -6,14 +6,17 @@ tbl_eprints <- readRDS(file.path(outdir, "tbl_eprints.rds"))
 tbl_authorkeys <- readRDS(file.path(outdir, "tbl_authorkeys.rds"))
 
 # dmls <- tbl_subjects %>% filter(subjects==10124) %>% left_join(tbl_eprints)
-geo <- tbl_subjects %>% filter(subjects==10122) %>% left_join(tbl_eprints) %>%
+geo <- tbl_subjects %>% filter(subjects==10124) %>% left_join(tbl_eprints) %>%
   left_join(tbl_authorkeys)
 
 tg <- table(geo$authorkey)
-sort(tg, decreasing = TRUE)[1:20]
-grep("schaepman", names(tg), value=TRUE)
+sort(tg, decreasing = TRUE)
+grep("robinson m", names(tg), value=TRUE)
 
-paste0(names(nm[]),collapse=",")
+mr <- grep("robinson m", names(tg))
+
+
+# paste0(names(nm[]),collapse=",")
 
 tbl_authors
 

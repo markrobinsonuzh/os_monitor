6a131455a253d79ff7c3b3f7fe11fac1953ecee2
retogerber <retgerb@gmail.com>
2020-12-07 10:32:27 +0000

    setup reverse proxy for hosting, unpaywall update adapted

--- update_unpaywall_sql.sh
+++ update_unpaywall_sql.sh
@@ -1,25 +1,99 @@
 #!/bin/bash
+# script to automatically download changefiles from unpaywall and update the local postgresql database
 
-wget -O /home/data_feed_main.json --no-check-certificate https://api.unpaywall.org/feed/changefiles?api_key=ibDEKRZbA2
-mapfile -t urls_arr < <( jq -cr '.[] | sort_by(.from_date) | reverse | .[]  |  .url | match(".*jsonl.*") | .string' /home/data_feed_main.json | head -n 1)
+# number of most recent files to insert into database:
+NRFILES=3
+INTERVAL=week
+if [ "$INTERVAL" = "week" ];
+then
+	# download overview file of all changefiles
+	wget -O /home/data_feed_main.json --no-check-certificate https://api.unpaywall.org/feed/changefiles?api_key=ibDEKRZbA2
+	# parse filenames into array
+	mapfile -t urls_arr < <( jq -cr '.[] | sort_by(.from_date) | reverse | .[]  |  .url | match(".*jsonl.*") | .string' /home/data_feed_main.json | head -n $NRFILES | tac)
+else
+        # download overview file of all changefiles
+	wget -O /home/data_feed_main.json --no-check-certificate "https://api.unpaywall.org/feed/changefiles?api_key=ibDEKRZbA2&interval=day"
+        # parse filenames into array
+        mapfile -t urls_arr < <( jq -cr '.[] | sort_by(.from_date) | reverse | .[]  |  .url | match(".*jsonl.*") | .string' /home/data_feed_main.json | tail -n $NRFILES )
+fi
+# loop through filenames
 for i in "${urls_arr[@]}"
 do
 	echo "download file: $i"
-	wget -O /home/data_feed.json.gz --no-check-certificate ${i} 
-	echo "gunzip file " 
-	gunzip -f /home/data_feed.json.gz 
-	echo "jq for processing" 
-	# jq -c '{doi: .doi | ascii_downcase, oa_status: .oa_status}' /home/data_feed.json > /home/data_feed_sub.json 
-	#jq -r ".doi | ascii_downcase, .oa_status " /home/data_feed.json | awk '{if(NR%2!=0) printf("%-10s, ",$0); else printf("%-10s ",$0); if(NR%2==0) printf("\n");}' > /home/data_feed.csv
-	 jq -r '{doi: .doi | ascii_downcase, oa_status: .oa_status} | .doi , .oa_status' /home/data_feed.json | awk '{if(NR%2!=0) printf("%-10s ",$0); else printf("%-10s",$0); if(NR%2==0) printf("\n");}' | awk '{$1=$1;print}' > /home/data_feed.csv
+	wget -q -O /home/data_feed.json.gz --no-check-certificate ${i}
+	status1=$?
+	if [ $status1 -eq 0 ];
+	then 
+		echo "        download was successful" 
+	else 
+		echo "        download failed with exit code $status1"
+		exit 1
+	fi
 
-	# run sql here
-	psql -U shiny -d oa -a -f /home/upsert_unpaywall.sql 	
+	echo "    $(date) gunzip file " 
+	gunzip -f /home/data_feed.json.gz
+	status2=$?
+        if [ $status2 -eq 0 ];
+        then
+                echo "        gunzip was successful"
+        else
+                echo "        gunzip failed with exit code $status2"
+                exit 1
+        fi
 	
-	#rm /home/data_feed_sub.json 
-	#rm /home/data_feed.json 
+	echo "    $(date) change \\\" to \" for correct parsing"
+	#sed -i 's/\\\\/\\/g' /home/data_feed.json
+	sed -i 's/\$\$[^$]*\$\$//g' /home/data_feed.json
+        status22=$?
+        if [ $status22 -eq 0 ];
+        then
+                echo "        sed was successful"
+        else
+                echo "        sed failed with exit code $status22"
+                exit 1
+        fi
+
+	echo "    $(date) remove latex equations"
+	sed -i 's/\$[^$]*\$//g' /home/data_feed.json
+        status23=$?
+        if [ $status23 -eq 0 ];
+        then
+                echo "        sed was successful"
+        else
+                echo "        sed failed with exit code $status23"
+                exit 1
+        fi
+
+	echo "    $(date) jq for processing"
+        # process json format into two column (doi and oa status) csv file (not comma separated but single whitespace separated, important for insertion into db)	
+	jq -r '{doi: .doi | ascii_downcase, oa_status: .oa_status} | .doi , .oa_status' /home/data_feed.json | awk '{if(NR%2!=0) printf("%-10s ",$0); else printf("%-10s",$0); if(NR%2==0) printf("\n");}' | awk '{$1=$1;print}' > /home/data_feed.csv
+        status3=$?
+        if [ $status3 -eq 0 ];
+        then
+                echo "        jq processing was successful"
+        else
+                echo "        jq processing failed with exit code $status3"
+                exit 1
+        fi
+
+	# run sql script to insert
+	echo "    $(date) insert into postgresql"
+	psql -U shiny -d oa -a -f /home/upsert_unpaywall.sql
+        status4=$?
+        if [ $status4 -eq 0 ];
+        then
+                echo "        postgresql insertion was successful" 
+        else
+                echo "        postgresql insertion failed with exit code $status4"
+                exit 1
+        fi
+	
 	echo "file ${i} successfully inserted into postgresql"
-	echo "..."	
+	echo "..............................................."
+	echo ""	
 done
 echo "done"
-
+echo "remove downloaded files"
+rm /home/data_feed_main.json 
+rm /home/data_feed.json 
+rm /home/data_feed.csv
--- upsert_unpaywall.sql
+++ upsert_unpaywall.sql
@@ -1,8 +1,8 @@
-
-
+/*PostgreSQL script to upsert unpaywall table (not fast so far since index cannot be removed when upserting)*/
+/*Temporary tables for data cleaning*/
 CREATE TEMPORARY TABLE unpaywall_copy_2 as (SELECT * FROM oa.unpaywall LIMIT 0);
 CREATE TEMPORARY TABLE unpaywall_copy as (SELECT * FROM oa.unpaywall LIMIT 0);
-
+/*Read data into second temp table*/
 COPY unpaywall_copy_2 FROM '/home/data_feed.csv'
 DELIMITER ' ';
 /* Remove dublicates */
@@ -11,48 +11,18 @@ SELECT
     DISTINCT ON (doi) doi,
     oa_status
 FROM unpaywall_copy_2; 
-
+/*Remove temporary table*/
 DROP TABLE unpaywall_copy_2;
-
-/*
-CREATE INDEX tmp_doi_unp_idx ON unpaywall_copy USING HASH (doi);
-
-
-SET search_path TO oa, public;
-
-UPDATE unpaywall
-SET oa_status = unpaywall_copy.oa_status
-FROM unpaywall_copy
-WHERE unpaywall.doi = unpaywall_copy.doi;
-
-
-DROP INDEX doi_unique;
-DROP INDEX doi_unp_idx;
-
-INSERT INTO oa.unpaywall(doi,oa_status)
-VALUES
-	(SELECT *
-	FROM   unpaywall_copy l
-	WHERE  NOT EXISTS (
-   	   SELECT  *
-	   FROM   oa.unpaywall
-	   WHERE  doi = l.doi
-	   );
-	)
-;
-
-CREATE INDEX doi_unp_idx ON oa.unpaywall USING HASH (doi);
-DROP INDEX tmp_doi_unp_idx;
-*/
-
+/*Set memory limit*/
 SET work_mem = '256MB';
-EXPLAIN ANALYSE INSERT INTO oa.unpaywall (doi, oa_status)
+/*Upsert into unpaywall table*/
+INSERT INTO oa.unpaywall (doi, oa_status)
 SELECT doi,oa_status 
 FROM unpaywall_copy
 ON CONFLICT (doi)
 DO
    UPDATE SET oa_status = EXCLUDED.oa_status;
-
+/*Remove temporary table*/
 DROP TABLE unpaywall_copy;
-
+/*Reset memory limit*/
 SET work_mem TO DEFAULT;

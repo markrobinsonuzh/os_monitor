b3b7c42f48243cca755699bbb6d0b9b5b56f0723
retogerber <retgerb@gmail.com>
2020-12-30 08:55:58 +0000

    change path of doc file

--- general_oa_app/app.R
+++ general_oa_app/app.R
@@ -2,4 +2,4 @@
 # setwd("/srv/shiny-server/")
 # maindir <- getwd()
 # devtools::load_all(file.path(maindir,"uzhOS"))
-uzhOS::shinyApp_general(con = odbc::dbConnect(odbc::odbc(), "PostgreSQL"))
+uzhOS::shinyApp_general(con = odbc::dbConnect(odbc::odbc(), "PostgreSQL"), docfile = "/srv/shiny-server/uzhOS/inst/extdata/helpfiles/OA_monitor_documentation.Rmd")
